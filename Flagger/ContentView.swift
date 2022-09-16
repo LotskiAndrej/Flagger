9f18c63d8d6a6aadac81ed0b838080bece49fce8
Andrej Lotski <andrej@2coders.com>
2022-09-16 10:45:56 +0200

    Initial Commit

---
+++ Flagger/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Flagger
+//
+//  Created by Andrej Lotski on 16.9.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Flagger/FlaggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlaggerApp.swift
+//  Flagger
+//
+//  Created by Andrej Lotski on 16.9.22.
+//
+
+import SwiftUI
+
+@main
+struct FlaggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
